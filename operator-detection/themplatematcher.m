c173bce7aa37978eb4cf7d7055e34b616587ec54
brunovanvliet <84525906+brunovanvliet@users.noreply.github.com>
2023-12-05 16:00:32 +0100

    Update themplatematcher.m

--- operator-detection/themplatematcher.m
+++ operator-detection/themplatematcher.m
@@ -1,21 +1,23 @@
+
 imRGB=imread('capture.bmp');
 BW = glovemask2(imRGB);
 imRGB=imread('capture1.bmp');
 BW1 = glovemask2(imRGB);
 imshow(BW1);
-biggest =0.0;
-
+biggest =uint32(0);
+V=uint32(0);
 posx=uint32(0);
 posy=uint32(0);
-for i =1:100:size(BW,1)-size(BW1,1)
 
-for j =1:100:size(BW,2)-size(BW1,2) 
+for i =1:100:size(BW,1)
+
+for j =1:100:size(BW,2)
   
 % Construct the correlation map
-imshow(BW(i:i+size(BW1,1)-1,j:j+size(BW1,2)-1));
 
 
-V =corr2(BW(i:i+size(BW1,1)-1,j:j+size(BW1,2)-1),BW1);
+
+V =calcCorr(BW,BW1,i,j);
 
 if V > biggest
     biggest =V;
@@ -51,10 +53,10 @@ for i =boundsx2:50:boundsx1
 for j =boundsy2:50:boundsy1 
   
 % Construct the correlation map
-imshow(BW(i:i+size(BW1,1)-1,j:j+size(BW1,2)-1));
 
 
-V =corr2(BW(i:i+size(BW1,1)-1,j:j+size(BW1,2)-1),BW1);
+
+V =calcCorr(BW,BW1,i,j);
 
 if V > biggest
     biggest =V;
@@ -90,10 +92,7 @@ for i =boundsx2:10:boundsx1
 for j =boundsy2:10:boundsy1 
   
 % Construct the correlation map
-imshow(BW(i:i+size(BW1,1)-1,j:j+size(BW1,2)-1));
-
-
-V =corr2(BW(i:i+size(BW1,1)-1,j:j+size(BW1,2)-1),BW1);
+V =calcCorr(BW,BW1,i,j);
 
 if V > biggest
     biggest =V;
@@ -129,10 +128,7 @@ for i =boundsx2:3:boundsx1
 for j =boundsy2:3:boundsy1 
   
 % Construct the correlation map
-imshow(BW(i:i+size(BW1,1)-1,j:j+size(BW1,2)-1));
-
-
-V =corr2(BW(i:i+size(BW1,1)-1,j:j+size(BW1,2)-1),BW1);
+V =calcCorr(BW,BW1,i,j);
 
 if V > biggest
     biggest =V;
@@ -168,10 +164,7 @@ for i =boundsx2:1:boundsx1
 for j =boundsy2:1:boundsy1 
   
 % Construct the correlation map
-imshow(BW(i:i+size(BW1,1)-1,j:j+size(BW1,2)-1));
-
-
-V =corr2(BW(i:i+size(BW1,1)-1,j:j+size(BW1,2)-1),BW1);
+V =calcCorr(BW,BW1,i,j);
 
 if V > biggest
     biggest =V;
@@ -182,4 +175,32 @@ end
  
 end
 end
+
 imshow(BW(posx:posx+size(BW1,1)-1,posy:posy+size(BW1,2)-1));
+
+biggestfactor = cast(biggest,"double")/(size(BW1,1)*size(BW1,2));
+
+function corr = calcCorr(A,Bt,positionx,positiony)
+sum =uint32(0);
+
+if(size(Bt,1)+positionx>size(A,1))
+disp("trigger activated1");
+corr=0;
+return;
+
+end
+if(size(Bt,2)+positiony>size(A,2))
+disp("trigger activated2");
+corr=0;
+return;
+end
+for k =(1+positionx):1:(size(Bt,1)+positionx)
+
+for l =(1+positiony):1:(size(Bt,2)+positiony)
+if(A(k,l)==Bt(k-positionx,l-positiony))
+sum = sum+1;
+end
+end
+end
+corr = sum;
+end
