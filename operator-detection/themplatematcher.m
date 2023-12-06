5387b8b393ee9e57a331c5eeebb1697228b870eb
ennozethof <152410538+ennozethof@users.noreply.github.com>
2023-12-06 01:48:55 +0100

    Update themplatematcher.m

--- operator-detection/themplatematcher.m
+++ operator-detection/themplatematcher.m
@@ -1,206 +1,60 @@
+imRGB = imread('c1.bmp');
+tempRGB = imread('template.bmp');
 
-imRGB=imread('capture.bmp');
-BW = glovemask2(imRGB);
-imRGB=imread('capture1.bmp');
-BW1 = glovemask2(imRGB);
-imshow(BW1);
-biggest =uint32(0);
-V=uint32(0);
-posx=uint32(0);
-posy=uint32(0);
+im = gloveMask3(imRGB);
+temp = gloveMask3(tempRGB);
 
-for i =1:100:maxx
+topCorr = 0;
+x = 0;
+y = 0;
 
-for j =1:100:maxy
-  
-% Construct the correlation map
+for i = 1:100:size(im,1) - size(temp,1)
+    for j = 1:100:size(im,2) - size(temp,2)
+        corr = calcCorr(im,temp,i,j);
+        if corr > topCorr
+            topCorr = corr;
+            x = i;
+            y = j;
+        end
+    end
+end
 
+[x, y, topCorr] = search(100, x, y, im, temp, topCorr);
+[x, y, topCorr] = search(50, x, y, im, temp, topCorr);
+[x, y, topCorr] = search(10, x, y, im, temp, topCorr);
+[x, y, topCorr] = search(3, x, y, im, temp, topCorr);
 
+imshow(im(x:x + size(temp,1),y:y + size(temp,2)));
+corrFactor = topCorr / (size(temp,1) * size(temp,2));
 
-V =calcCorr(BW,BW1,i,j);
+function corr = calcCorr(im,temp,x,y)
 
-if V > biggest
-    biggest =V;
-    posx=i;
-    posy=j;
+    corr = 0;
+    for i = 1:1:size(temp,1)
+        for j = 1:1:size(temp,2)
+            if (im(x + i, y + j) == temp(i,j))
+                corr = corr + 1;
+            end
+        end
+    end
+end
+
+function [x, y, topCorr] = search(checkrange, x, y, im, temp, topCorr)
+
+    lowerX = max((x - checkrange), 1);
+    upperX = min((x + checkrange), (size(im,1) - size(temp,1)));
+
+    lowerY = max((y - checkrange), 1);
+    upperY = min((y + checkrange), (size(im,2) - size(temp,2)));
     
-end
- 
-end
-end
-maxx =size(BW,1)-size(BW1,1);
-maxy =size(BW,2)-size(BW1,2);
-checkrange =uint32(100);
-boundsx1=uint32(posx+checkrange);
-boundsx2=uint32(posx-checkrange);
-boundsy1=uint32(posy+checkrange);
-boundsy2=uint32(posy-checkrange);
-if(boundsx1>maxx)
-boundsx1=maxx;
-end
-if(boundsy1>maxy)
-boundsy1=maxy;
-end
-if(boundsx2<=0)
-boundsx2=1;
-end
-if(boundsy2<=0)
-boundsy2=1;
-end
-
-for i =boundsx2:50:boundsx1
-
-for j =boundsy2:50:boundsy1 
-  
-% Construct the correlation map
-
-
-
-V =calcCorr(BW,BW1,i,j);
-
-if V > biggest
-    biggest =V;
-    posx=i;
-    posy=j;
-    
-end
- 
-end
-end
-maxx =size(BW,1)-size(BW1,1);
-maxy =size(BW,2)-size(BW1,2);
-checkrange =uint32(50);
-boundsx1=uint32(posx+checkrange);
-boundsx2=uint32(posx-checkrange);
-boundsy1=uint32(posy+checkrange);
-boundsy2=uint32(posy-checkrange);
-if(boundsx1>maxx)
-boundsx1=maxx;
-end
-if(boundsy1>maxy)
-boundsy1=maxy;
-end
-if(boundsx2<=0)
-boundsx2=1;
-end
-if(boundsy2<=0)
-boundsy2=1;
-end
-
-for i =boundsx2:10:boundsx1
-
-for j =boundsy2:10:boundsy1 
-  
-% Construct the correlation map
-V =calcCorr(BW,BW1,i,j);
-
-if V > biggest
-    biggest =V;
-    posx=i;
-    posy=j;
-    
-end
- 
-end
-end
-maxx =size(BW,1)-size(BW1,1);
-maxy =size(BW,2)-size(BW1,2);
-checkrange =uint32(10);
-boundsx1=uint32(posx+checkrange);
-boundsx2=uint32(posx-checkrange);
-boundsy1=uint32(posy+checkrange);
-boundsy2=uint32(posy-checkrange);
-if(boundsx1>maxx)
-boundsx1=maxx;
-end
-if(boundsy1>maxy)
-boundsy1=maxy;
-end
-if(boundsx2<=0)
-boundsx2=1;
-end
-if(boundsy2<=0)
-boundsy2=1;
-end
-
-for i =boundsx2:3:boundsx1
-
-for j =boundsy2:3:boundsy1 
-  
-% Construct the correlation map
-V =calcCorr(BW,BW1,i,j);
-
-if V > biggest
-    biggest =V;
-    posx=i;
-    posy=j;
-    
-end
- 
-end
-end
-maxx =size(BW,1)-size(BW1,1);
-maxy =size(BW,2)-size(BW1,2);
-checkrange =uint32(3);
-boundsx1=uint32(posx+checkrange);
-boundsx2=uint32(posx-checkrange);
-boundsy1=uint32(posy+checkrange);
-boundsy2=uint32(posy-checkrange);
-if(boundsx1>maxx)
-boundsx1=maxx;
-end
-if(boundsy1>maxy)
-boundsy1=maxy;
-end
-if(boundsx2<=0)
-boundsx2=1;
-end
-if(boundsy2<=0)
-boundsy2=1;
-end
-
-for i =boundsx2:1:boundsx1
-
-for j =boundsy2:1:boundsy1 
-  
-% Construct the correlation map
-V =calcCorr(BW,BW1,i,j);
-
-if V > biggest
-    biggest =V;
-    posx=i;
-    posy=j;
-    
-end
- 
-end
-end
-
-imshow(BW(posx:posx+size(BW1,1)-1,posy:posy+size(BW1,2)-1));
-
-biggestfactor = cast(biggest,"double")/(size(BW1,1)*size(BW1,2));
-
-function corr = calcCorr(A,Bt,positionx,positiony)
-sum =uint32(0);
-
-if(size(Bt,1)+positionx>size(A,1))
-disp("trigger activated1");
-corr=0;
-return;
-
-end
-if(size(Bt,2)+positiony>size(A,2))
-disp("trigger activated2");
-corr=0;
-return;
-end
-for k =(1+positionx):1:(size(Bt,1)+positionx)
-
-for l =(1+positiony):1:(size(Bt,2)+positiony)
-if(A(k,l)==Bt(k-positionx,l-positiony))
-sum = sum+1;
-end
-end
-end
-corr = sum;
+    for i = lowerX:checkrange:upperX
+        for j = lowerY:checkrange:upperY
+            corr = calcCorr(im,temp,i,j);
+            if corr > topCorr
+                topCorr = corr;
+                x = i;
+                y = j;
+            end
+        end
+    end
 end
