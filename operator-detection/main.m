388247b77b0ecda5cd4811076b82c077e744b3d7
ennozethof <152410538+ennozethof@users.noreply.github.com>
2023-12-07 18:54:39 +0100

    Update main.m

--- operator-detection/main.m
+++ operator-detection/main.m
@@ -1,12 +1,11 @@
 templates = ["plusBW.bmp", "minusBW.bmp", "multiplyBW.bmp", "devideBW.bmp"];
+operands = ["Plus", "Minus", "Multiplication", "Devision"];
+
 plusTemp = imread(templates(1));
 minusTemp = imread(templates(2));
 multiplyTemp = imread(templates(3));
 devideTemp = imread(templates(4));
-
-fingerTemp = imread("template3.bmp");
-
-operands = ["Plus", "Minus", "Multiplication", "Devision"];
+fingerTemp = imread("fingerBW.bmp");
 
 vid = videoinput('winvideo',1);
 set(vid,'ReturnedColorSpace','rgb');
@@ -15,7 +14,7 @@ preview(vid);
 
 while true
 
-    pause(5);
+    pause(3);
     im = getsnapshot(vid);
 
     imwrite(im,'tempcapture.bmp','bmp');
@@ -24,16 +23,29 @@ while true
     subplot(3,4,1);
     imshow(im);
     title('RGB image');
-    
+
     im = gloveMask3(im);
     subplot(3,4,2);
     imshow(im);
     title('BW image');
     
     im = imfill(im, 8, "holes");
+    im = imopen(im, strel('disk', 4));
+    [r,c] = find(im);
+
+    if (size(r, 1) == 0) || (size(c, 1) == 0)
+        disp("nothing detected, end of sequence\n");
+        break;
+    end
+
+    r1 = max(1, min(r) - 100);
+    r2 = min(size(im,1), max(r) + 100);
+    c1 = max(1, min(c) - 100);
+    c2 = min(size(im,2), max(c) + 100);
+    im = im(r1:r2,c1:c2);
     subplot(3,4,3);
     imshow(im);
-    title('Filled holes');
+    title('Clean and cropped');
 
     [plusCorr, x1, y1] = tempMatcher(im, plusTemp);
     [minusCorr, x2, y2] = tempMatcher(im, minusTemp);
@@ -53,21 +65,16 @@ while true
 
     x = xs(i);
     y = ys(i);
-    imcropped = im(x:x + size(temp,1),y:y + size(temp,2));
+    imcropped = im(x:min(size(im,1), x + size(temp,1)),y:min(size(im,2), y + size(temp,2)));
     subplot(3,4,7);
     imshow(imcropped);
-    title(sprintf('Highest correlation on image: %d', corrFactor));
+    title(sprintf('Highest correlation on image: %d', corr));
 
-    if corr > 0.8
+    if corr > 0.9
         fprintf('%s sign, corr: %d\n', operands(i), corr);
     else
         numFingers = countFingers(im, fingerTemp);
-        if numFingers > 0
-            fprintf('%d fingers\n', numFingers);
-        else
-            disp("nothing detected, end of sequence\n");
-            break;
-        end
+        fprintf('%d fingers\n', numFingers);
     end
 end
 
