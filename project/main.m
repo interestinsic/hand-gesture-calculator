4efd7df256894507ba5f10c0192bedb6e18d6221
interestinsic <105859890+interestinsic@users.noreply.github.com>
2023-12-17 19:18:01 +0100

    Update main.m

--- project/main.m
+++ project/main.m
@@ -1,6 +1,8 @@
+% Load templates for arithmetic operations and fingers
 templates = ["plusBW.bmp", "minusBW.bmp", "multiplyBW.bmp", "devideBW.bmp"];
 operands = ["+", "-", "*", "/"];
 
+% Read each template image from the file
 plusTemp = imread(templates(1));
 minusTemp = imread(templates(2));
 multiplyTemp = imread(templates(3));
@@ -10,41 +12,50 @@ fingerTemp = imread("fingerBW.bmp");
 expression = "";
 valid = true;
 
+% Set up the video input from the webcam
 vid = videoinput('winvideo',1);
 set(vid,'ReturnedColorSpace','rgb');
 start(vid);
 preview(vid);
 
+% Loop to continuously capture images
 while true
 
     for i = 3:-1:1
+        % Countdown for next picture capture
         fprintf('Next picture in:  %d\n', i);
         pause(1);
     end
 
     im = getsnapshot(vid);
 
+    % Store image locally
     imwrite(im,'tempcapture.bmp','bmp');
     im = imread('tempcapture.bmp');
 
+    % Display the original RGB image
     subplot(3,4,1);
     imshow(im);
     title('RGB image');
 
+    % Apply the glove color thresholding function and get a Binary image in return
     im = gloveMask(im);
     subplot(3,4,2);
     imshow(im);
     title('BW image');
-    
+
+    % Fill holes and remove noise from the binary image
     im = imfill(im, 8, "holes");
     im = imopen(im, strel('disk', 4));
     [r,c] = find(im);
 
+    % Check if there are any detected objects, otherwise end the loop
     if (size(r, 1) == 0) || (size(c, 1) == 0)
         disp("Nothing detected, end of sequence");
         break;
     end
 
+    % Crop the image to the region of interest
     r1 = max(1, min(r) - 100);
     r2 = min(size(im,1), max(r) + 100);
     c1 = max(1, min(c) - 100);
@@ -54,22 +65,26 @@ while true
     imshow(im);
     title('Clean and cropped');
 
+    % Use template matching to find correlation with each operator
     [plusCorr, x1, y1] = tempMatcher(im, plusTemp);
     [minusCorr, x2, y2] = tempMatcher(im, minusTemp);
     [multiplyCorr, x3, y3] = tempMatcher(im, multiplyTemp);
     [devideCorr, x4, y4] = tempMatcher(im, devideTemp);
 
+    % Find the highest correlation of all operator templates
     corrs = [plusCorr, minusCorr, multiplyCorr, devideCorr];
     [corr, i] = max(corrs);
 
     xs = [x1, x2, x3, x4];
     ys = [y1, y2, y3, y4];
 
+    % Load and display the template with the highest correlation
     temp = imread(templates(i));
     subplot(3,4,5);
     imshow(temp);
     title(sprintf('Highest correlation template: %s', operands(i)));
 
+    % Crop the image around the region of the highest correlation
     x = xs(i);
     y = ys(i);
     imcropped = im(x:min(size(im,1), x + size(temp,1)),y:min(size(im,2), y + size(temp,2)));
@@ -88,6 +103,7 @@ while true
         expression = strcat(expression, operands(i));
         delete(subplot(3,4,9:12));
     else
+        % Detect the number of fingers and add it to the expression
         numFingers = countFingers(im, fingerTemp);
         if numFingers > 0
             fprintf('Number: %d\n', numFingers);
