21241e975c8554f0db8638581e0c0dc5b50b92cf
interestinsic <105859890+interestinsic@users.noreply.github.com>
2023-12-17 19:05:38 +0100

    Update countFingers.m

--- project/countFingers.m
+++ project/countFingers.m
@@ -4,17 +4,21 @@ subplot(3,4,9);
 imshow(temp);
 title('Finger template');
 
+% Morphological opening on the image using the finger template
 im2 = imopen(im,strel(temp));
 im = im - im2;
+% Subtracting the opened image from the original to enhance finger regions
 subplot(3,4,10);
 imshow(im);
 title('Template applied');
 
+% Remove small objects or noise not removed in the previous step
 im = imopen(im, strel('disk',16));
 subplot(3,4,11);
 imshow(im);
 title('Noise removed');
 
+% Labeling connected components (potential fingers) in the image
 [labels,numlabels] = bwlabel(im);
 im = label2rgb(labels);
 subplot(3,4,12);
