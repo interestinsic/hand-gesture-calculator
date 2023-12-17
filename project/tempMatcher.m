86a84a954c84f73967612f2d8e29e88b363a1c47
interestinsic <105859890+interestinsic@users.noreply.github.com>
2023-12-17 19:25:43 +0100

    Update tempMatcher.m

--- project/tempMatcher.m
+++ project/tempMatcher.m
@@ -1,9 +1,11 @@
+% Function to find the highest correlation to the given operator template
 function [topCorr, x, y] = tempMatcher(im, temp)
 
     topCorr = 0;
     x = 1;
     y = 1;
-    
+
+    % Return if the template is larger than the image
     if (size(im,1) < size(temp, 1)) || (size(im,2) < size(temp, 2))
         return
     end
@@ -27,18 +29,22 @@ function [topCorr, x, y] = tempMatcher(im, temp)
     [x, y, topCorr] = search(1, x, y, im, temp, topCorr);
 end
 
+% A helper function to search for the best match within a specified range
 function [x, y, topCorr] = search(checkrange, x, y, im, temp, topCorr)
 
+    % Determine the search bounds ensuring they stay within the image
     lowerX = max((x - checkrange), 1);
     upperX = min((x + checkrange), (size(im,1) - size(temp,1)));
 
     lowerY = max((y - checkrange), 1);
     upperY = min((y + checkrange), (size(im,2) - size(temp,2)));
-    
+
+    % Scan the area around the best match found so far
     for i = lowerX:checkrange:upperX
         for j = lowerY:checkrange:upperY
             emplaced = im(i:i+size(temp, 1) - 1, j:j+size(temp, 2) - 1);
             corr = dice(emplaced, temp);
+            % Update the best correlation and its position if a better match is found
             if corr > topCorr
                 topCorr = corr;
                 x = i;
